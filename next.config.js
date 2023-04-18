827ccf53f092d87d78d0b8a8d773ce417b70cc4a
R-Tsukada <r.tsukada14@gmail.com>
2023-04-18 18:05:15 +0900

    srcディレクトリが機能するようにする
    参考: https://beta.nextjs.org/docs/upgrade-guide#step-1-creating-the-app-directory

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  output: 'standalone',
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
 };
+
+module.exports = nextConfig;
