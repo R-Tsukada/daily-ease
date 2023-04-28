d1edead70589f1b063ef187db0efa3504a935871
R-Tsukada <r.tsukada14@gmail.com>
2023-04-28 13:50:09 +0900

    ナビゲーションバーのレイアウトを修正する

--- src/container/templates/Navbar.tsx
+++ src/container/templates/Navbar.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Link from 'next/link'
 
-const Header = () => (
-  <div className='flex justify-between bg-blue-700'>
-    <div className='text-3xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-8 mt-8 pl-4'>
+const Navbar = () => (
+  <div className='flex justify-between bg-[#6066D0]'>
+    <div className='text-3xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight my-2 pl-3'>
       <Link href='/' className='hover:underline'>
-        <span className='text-white mr-1'>Daily</span><span className='text-orange-400'>Ease</span>
+        <span className='text-white mr-1 font-[Poppins]'>Daily</span><span className='text-orange-400 font-[Poppins]'>Ease</span>
       </Link>
     </div>
   </div>
 )
 
-export default Header
+export default Navbar
