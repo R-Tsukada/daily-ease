ec81b076af39f118d4e357104763f940473f2dd6
R-Tsukada <r.tsukada14@gmail.com>
2023-04-28 13:32:21 +0900

    ヘッダーを全ページで表示されるようにする
    - HeaderをNavbarに変更
    - 全ページで表示されるようにLayoutファイルを作成
    - Layoutを_app.tsxに加えた

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,15 @@
 import React from 'react'
 import Link from 'next/link'
 import PageSelect from '../container/templates/PageSelect'
-import Header from '../container/templates/Header'
 
 const Home = () =>
   (
-    <>
-      <Header />
-      <div className='flex items-center justify-center min-h-screen'>
-        <div>
-          <h2 className='text-2xl font-bold text-center mb-4'>Daily Ease</h2>
-          <Link href='/Questions'><span className='hover:enabled'><PageSelect /></span></Link>
-        </div>
+    <div className='flex items-center justify-center min-h-screen'>
+      <div>
+        <h2 className='text-2xl font-bold text-center mb-4'>Daily Ease</h2>
+        <Link href='/Questions'><span className='hover:enabled'><PageSelect /></span></Link>
       </div>
-    </>
+    </div>
   )
 
 export default Home
