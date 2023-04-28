ce294dcafffdcb58bdbdf2dbb1dcaf39c10c124b
R-Tsukada <r.tsukada14@gmail.com>
2023-04-28 18:27:11 +0900

    pagesにあったテストを__tests__に移行した
    今後のテストファイルをどこに置くかは検討する

--- styles/__tests__/Questions.test.tsx
+++ styles/__tests__/Questions.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { fireEvent, render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
-import Questions from './Questions'
+import Questions from '../../src/pages/Questions'
 
 describe('Questions', () => {
   beforeEach(() => {
