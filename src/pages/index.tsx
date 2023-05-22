a949c837bdfc4d9a5ec388de277d395dec37b6db
R-Tsukada <r.tsukada14@gmail.com>
2023-05-22 14:27:32 +0900

    メッセージを変更する

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ const Home = () =>
     <div className='flex justify-center min-h-screen'>
       <div className='mt-5'>
         <h2 className='text-3xl font-bold text-center my-2'>DailyEase</h2>
-        <p className='text-xl text-center my-2'>自分の状態を把握して、充実感のある毎日を凄そう</p>
+        <p className='text-xl text-center my-2'>自分の状態を把握して、充実感のある毎日を送ろう</p>
         <p className='text-xl text-center my-4'>気になる項目を選んでみよう👇</p>
         <div className='mt-10'>
           <Link href='/OlbiQuestions'><span className='hover:enabled'><StressCheckPageSelect /></span></Link>
