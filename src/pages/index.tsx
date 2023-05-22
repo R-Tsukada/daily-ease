f5ec65c8348d64be32f8676a2a1273c2d42f9ec2
R-Tsukada <r.tsukada14@gmail.com>
2023-05-22 13:39:27 +0900

    トップページのメッセージを変更する

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,9 +7,13 @@ const Home = () =>
   (
     <div className='flex justify-center min-h-screen'>
       <div className='mt-5'>
-        <h2 className='text-2xl font-bold text-center mb-4'>Daily Ease</h2>
-        <Link href='/OlbiQuestions'><span className='hover:enabled'><StressCheckPageSelect /></span></Link>
-        <Link href='/UwesQuestions'><span className='hover:enabled'><EnrichmentCheckPageSelect /></span></Link>
+        <h2 className='text-3xl font-bold text-center my-2'>DailyEase</h2>
+        <p className='text-xl text-center my-2'>自分の状態を把握して、充実感のある毎日を凄そう</p>
+        <p className='text-xl text-center my-4'>気になる項目を選んでみよう👇</p>
+        <div className='mt-10'>
+          <Link href='/OlbiQuestions'><span className='hover:enabled'><StressCheckPageSelect /></span></Link>
+          <Link href='/UwesQuestions'><span className='hover:enabled'><EnrichmentCheckPageSelect /></span></Link>
+        </div>
       </div>
     </div>
   )
