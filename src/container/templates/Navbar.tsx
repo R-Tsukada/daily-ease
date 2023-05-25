35099a027389589195e6db38f02060c15ea3e3cc
R-Tsukada <r.tsukada14@gmail.com>
2023-05-25 21:54:22 +0900

    アプリ名がセンターになるようにする

--- src/container/templates/Navbar.tsx
+++ src/container/templates/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 const Navbar = () => (
   <div className='flex justify-between bg-[#6066D0]'>
-    <div className='text-3xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight my-2 pl-3'>
+    <div className='text-2xl md:text-3xl font-semibold tracking-tight md:tracking-tighter leading-tight mx-auto py-2'>
       <Link href='/' className='hover:underline'>
         <span className='text-white mr-1 font-[Poppins]'>Daily</span><span className='text-orange-400 font-[Poppins]'>Ease</span>
       </Link>
