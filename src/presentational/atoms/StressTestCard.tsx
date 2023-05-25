a1afcd46ba722f7a79e317327d193a9de2398628
R-Tsukada <r.tsukada14@gmail.com>
2023-05-25 17:49:47 +0900

    テストに移行するボタンのレイアウトを変更する

--- src/presentational/atoms/StressTestCard.tsx
+++ src/presentational/atoms/StressTestCard.tsx
@@ -5,11 +5,10 @@ interface CardProps {
 }
 
 const QuestionsCard: React.FC<CardProps> = ({ children }) => (
-  <div className="max-w-sm w-full lg:max-w-full lg:flex">
-    <div className="border rounded-xl border-gray-400 bg-red-500 p-4 my-4">
-      <div>
-        <div className="text-center text-white font-bold text-xl">{ children }</div>
-      </div>
+  <div className="lg:max-w-full lg:flex">
+    <div className="w-72 h-24 border rounded-xl border-gray-300 bg-gradient-to-r from-red-500 to-red-300 mx-auto flex flex-col">
+      <p className="text-white font-medium text-sm ml-2 my-auto">{ children }</p>
+      <p className='text-xs text-white border-t-2 border-white w-full py-1 pr-2 text-right'>所要時間2分 全8問</p>
     </div>
   </div>
 )
